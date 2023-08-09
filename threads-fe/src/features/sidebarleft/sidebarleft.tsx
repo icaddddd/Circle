533d0286fe11ec2eb29ed913d4726912fc4f55d0
icad <rhisjaddjvtr@gmail.com>
2023-08-10 00:54:06 +0700

    first

--- threads-fe/src/features/sidebarleft/sidebarleft.tsx
+++ threads-fe/src/features/sidebarleft/sidebarleft.tsx
@@ -3,6 +3,7 @@ import { Box, Button, Text } from "@chakra-ui/react";
 import { AiFillHome, AiOutlineSearch } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
 import { RiUserFollowFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return(
@@ -10,7 +11,9 @@ export default function Navbar() {
         <Box gap={4} p={5} my={7} mx={1}>
           <Text fontSize={50} color={"green.500"} fontWeight={"bold"}>Circle</Text>
           <Box my={5}>
-            <Text fontSize={25} my={3}><Icon as={AiFillHome} mr={3}></Icon>Home</Text>
+            <Link to={"/"}>
+              <Text fontSize={25} my={3}><Icon as={AiFillHome} mr={3}></Icon>Home</Text>
+            </Link>
             <Text fontSize={25} my={3}><Icon as={AiOutlineSearch} mr={3}></Icon>Search</Text>
             <Text fontSize={25} my={3}><Icon as={RiUserFollowFill} mr={3}></Icon>Follows</Text>
             <Text fontSize={25} my={3}><Icon as={CgProfile} mr={3}></Icon>Profile</Text>
