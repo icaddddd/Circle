6b6cade0c6bd60553c25712968ac9e779e872132
icad <rhisjaddjvtr@gmail.com>
2023-11-09 20:54:19 +0700

    add vercel.json

--- threads-be/src/services/ThreadOrang.ts
+++ threads-be/src/services/ThreadOrang.ts
@@ -1,16 +1,21 @@
-import { Repository } from "typeorm"
-import { AppDataSource } from "../data-source"
-import { Thread } from "../entities/Thread"
+import { Repository } from "typeorm";
+import { AppDataSource } from "../data-source";
+import { Thread } from "../entities/Thread";
 
 export class ThreadOrang {
-    private readonly threadRepository: Repository<Thread> = AppDataSource.getRepository(Thread)
-    async findOne(req: any, res: any) {
-        const threadOrang = await this.threadRepository.findOne({
-            where: {
-                id: req.params.id
-            }
-        })
-        res.status(200).json(threadOrang)
+  private readonly threadRepository: Repository<Thread> =
+    AppDataSource.getRepository(Thread);
+  async findOne(req: any, res: any) {
+    try {
+      const threadOrang = await this.threadRepository.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json(threadOrang);
+    } catch (error) {
+      res.status(500).json({ error: error });
     }
+  }
 }
-export default new ThreadOrang()
+export default new ThreadOrang();
