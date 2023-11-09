0dfd1b38faf49a23c4419b43e8e4fe2399891c5d
icad <rhisjaddjvtr@gmail.com>
2023-11-09 21:26:02 +0700

    add vercel.json

--- threads-be/src/data-source.ts
+++ threads-be/src/data-source.ts
@@ -1,29 +1,29 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "sjfrhsjd",
-    database: "db_threads_be",
-    synchronize: true,
-    logging: false,
-    entities: ["src/entities/*.ts"],
-    migrations: ["src/migrations/*.ts"],
-    subscribers: [],
-})
 // export const AppDataSource = new DataSource({
-//   type: "postgres",
-//   host: "containers-us-west-146.railway.app",
-//   port: 6512,
-//   username: "postgres",
-//   password: "MFe37gWz8lm5MMVhmq2B",
-//   database: "railway",
-//   synchronize: true,
-//   logging: false,
-//   entities: ["src/entities/*.ts"],
-//   migrations: ["src/migrations/*.ts"],
-//   subscribers: [],
-// });
+//     type: "postgres",
+//     host: "localhost",
+//     port: 5432,
+//     username: "postgres",
+//     password: "sjfrhsjd",
+//     database: "db_threads_be",
+//     synchronize: true,
+//     logging: false,
+//     entities: ["src/entities/*.ts"],
+//     migrations: ["src/migrations/*.ts"],
+//     subscribers: [],
+// })
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "containers-us-west-146.railway.app",
+  port: 6512,
+  username: "postgres",
+  password: "MFe37gWz8lm5MMVhmq2B",
+  database: "railway",
+  synchronize: true,
+  logging: false,
+  entities: ["src/entities/*.ts"],
+  migrations: ["src/migrations/*.ts"],
+  subscribers: [],
+});
